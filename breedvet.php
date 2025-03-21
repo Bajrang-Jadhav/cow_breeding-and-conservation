8c59a4d16eeb959423cd14175f31f9a1deaed6bf
sumedh248 <160852535+sumedh248@users.noreply.github.com>
2025-03-21 12:55:42 +0530

    213

--- breedvet.php
+++ breedvet.php
@@ -108,7 +108,7 @@
                 <ol class="breadcrumb justify-content-center mb-0">
                     <li class="breadcrumb-item"><a href="#">Home</a></li>
                     <li class="breadcrumb-item"><a href="#">Pages</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">Gallery</li>
+                    <li class="breadcrumb-item active" aria-current="page">BREEDING AND VETERANARY</li>
                 </ol>
             </nav>
         </div>
@@ -116,74 +116,9 @@
     <!-- Page Header End -->
 
 
-    <!-- Gallery Start -->
-    <div class="container-xxl py-5">
-        <div class="container">
-            <div class="text-center mx-auto pb-4 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
-                <p class="section-title bg-white text-center text-primary px-3">Gallery</p>
-                <h1 class="mb-5">Explore Our Hack-a-Farm Gallery</h1>
-            </div>
-            <div class="row g-0">
-                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="row g-0">
-                        <div class="col-12">
-                            <a class="d-block" href="img/gallery-5.jpg" data-lightbox="gallery">
-                                <img class="img-fluid" src="img/gallery-5.jpg" alt="">
-                            </a>
-                        </div>
-                        <div class="col-12">
-                            <a class="d-block" href="img/gallery-1.jpg" data-lightbox="gallery">
-                                <img class="img-fluid" src="img/gallery-1.jpg" alt="">
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
-                    <div class="row g-0">
-                        <div class="col-12">
-                            <a class="d-block" href="img/gallery-2.jpg" data-lightbox="gallery">
-                                <img class="img-fluid" src="img/gallery-2.jpg" alt="">
-                            </a>
-                        </div>
-                        <div class="col-12">
-                            <a class="d-block" href="img/gallery-6.jpg" data-lightbox="gallery">
-                                <img class="img-fluid" src="img/gallery-6.jpg" alt="">
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
-                    <div class="row g-0">
-                        <div class="col-12">
-                            <a class="d-block" href="img/gallery-7.jpg" data-lightbox="gallery">
-                                <img class="img-fluid" src="img/gallery-7.jpg" alt="">
-                            </a>
-                        </div>
-                        <div class="col-12">
-                            <a class="d-block" href="img/gallery-3.jpg" data-lightbox="gallery">
-                                <img class="img-fluid" src="img/gallery-3.jpg" alt="">
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
-                    <div class="row g-0">
-                        <div class="col-12">
-                            <a class="d-block" href="img/gallery-4.jpg" data-lightbox="gallery">
-                                <img class="img-fluid" src="img/gallery-4.jpg" alt="">
-                            </a>
-                        </div>
-                        <div class="col-12">
-                            <a class="d-block" href="img/gallery-8.jpg" data-lightbox="gallery">
-                                <img class="img-fluid" src="img/gallery-8.jpg" alt="">
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Gallery End -->
+    <!-- BREEDING AND VETERANARY Start -->
+    
+    <!-- BREEDING AND VETERANARY End -->
 
 
     <!-- Footer Start -->
