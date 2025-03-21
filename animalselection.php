29182fcf622ff3a40634530d26f17020fec8a1a9
sumedh248 <160852535+sumedh248@users.noreply.github.com>
2025-03-21 19:48:48 +0530

    214

--- animalselection.php
+++ animalselection.php
@@ -103,11 +103,11 @@
     <!-- Page Header Start -->
     <div class="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
         <div class="container text-center py-5">
-            <h1 class="display-3 text-white mb-4 animated slideInDown">Gallery</h1>
+            <h1 class="display-3 text-white mb-4 animated slideInDown">ANIMAL SELECTION</h1>
             <nav aria-label="breadcrumb animated slideInDown">
                 <ol class="breadcrumb justify-content-center mb-0">
                     <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item"><a href="#">Pages</a></li>
+                    <li class="breadcrumb-item"><a href="#">Services</a></li>
                     <li class="breadcrumb-item active" aria-current="page">ANIMAL SELECTION</li>
                 </ol>
             </nav>
--- breedvet.php
+++ breedvet.php
@@ -103,11 +103,11 @@
     <!-- Page Header Start -->
     <div class="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
         <div class="container text-center py-5">
-            <h1 class="display-3 text-white mb-4 animated slideInDown">Gallery</h1>
+            <h1 class="display-3 text-white mb-4 animated slideInDown">BREEDING AND VETERANARY</h1>
             <nav aria-label="breadcrumb animated slideInDown">
                 <ol class="breadcrumb justify-content-center mb-0">
                     <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item"><a href="#">Pages</a></li>
+                    <li class="breadcrumb-item"><a href="#">Services</a></li>
                     <li class="breadcrumb-item active" aria-current="page">BREEDING AND VETERANARY</li>
                 </ol>
             </nav>
