915dda63d8a2f85f48daa5539df83893181e9836
Bajrang jadhav <167007697+Bajrang-Jadhav@users.noreply.github.com>
2025-04-04 00:22:56 +0530

    1

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include('send.php');
+include('config.php');
 
  //Import PHPMailer classes into the global namespace
 //These must be at the top of your script, not inside a function
